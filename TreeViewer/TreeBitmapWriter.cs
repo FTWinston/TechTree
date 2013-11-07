20115889edb4a413397beb56b7f7e64896bb062a
FTWinston <ftwinston@gmail.com>
2013-11-07 00:30:59 +0000

    TreeBitmapWriter now sorts nodes by tree depth, and renders them, though currently without prerequisites

--- TreeViewer/TreeBitmapWriter.cs
+++ TreeViewer/TreeBitmapWriter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using TechTree;
 
 namespace TreeViewer
 {
@@ -10,12 +11,66 @@ namespace TreeViewer
     {
         public static Bitmap WriteImage(TechTree.TechTree tree, int width, int height)
         {
+            var sortedNodes = SortByDepth(tree);
+            Brush treeNode = new SolidBrush(Color.DarkGray);
             Bitmap image = new Bitmap(width, height);
+
+            float nodeWidth = width * 0.5f / (sortedNodes.Count - 0.5f), nodeHeight = nodeWidth / 1.41421356f;
+            float depthIncrement = nodeWidth * 2f;
+            float insetIncrement = height / 8f;
+
             using (Graphics g = Graphics.FromImage(image))
             {
-                g.FillRectangle(new SolidBrush(Color.Red), 0, 0, width, height);
+                foreach (var nodeList in sortedNodes)
+                {
+                    int i = 0;
+                    foreach (var node in nodeList.Value)
+                    {
+                        g.FillEllipse(treeNode, nodeList.Key * depthIncrement, i * insetIncrement, nodeWidth, nodeHeight);
+                        i++;
+                    }
+                }
             }
             return image;
+        }
+
+        private static SortedList<int, List<TreeNode>> SortByDepth(TechTree.TechTree tree)
+        {
+            var nodesByDepth = new SortedList<int, List<TreeNode>>();
+
+            List<TreeNode> toProcess = new List<TreeNode>();
+            toProcess.Add(tree.RootNode);
+
+            while (toProcess.Count > 0)
+            {
+                TreeNode node = toProcess[0]; toProcess.RemoveAt(0);
+
+                int depth = Depth(node);
+                List<TreeNode> nodesAtDepth;
+                if (!nodesByDepth.TryGetValue(depth, out nodesAtDepth))
+                {
+                    nodesAtDepth = new List<TreeNode>();
+                    nodesByDepth.Add(depth, nodesAtDepth);
+                }
+                nodesAtDepth.Add(node);
+
+                foreach (TreeNode child in node.Unlocks)
+                    if (!toProcess.Contains(child))
+                        toProcess.Add(child);
+            }
+
+            return nodesByDepth;
+        }
+
+        private static int Depth(TreeNode node)
+        {
+            if ( node.Prerequisites.Count == 0 )
+                return 0;
+            
+            int depth = 0;
+            foreach (TreeNode parent in node.Prerequisites)
+                depth = Math.Max(depth, Depth(parent));
+            return depth + 1;
         }
     }
 }
