456ebda859d5bcf389a011c6add0b4204e9c973b
FTWinston <ftwinston@gmail.com>
2014-01-07 13:26:53 +0000

    Trees now drawn vertically instead of horizontally

--- TreeViewer/TreeBitmapWriter.cs
+++ TreeViewer/TreeBitmapWriter.cs
@@ -16,20 +16,20 @@ namespace TreeViewer
             Bitmap image = new Bitmap(width, height);
 
             float nodeWidth = width * 0.5f / (tree.MaxDepth + 0.5f), nodeHeight = Math.Min(nodeWidth / 1.41421356f, height * 0.5f / (tree.MaxRowPos + 0.5f));
-            float depthIncrement = nodeWidth * 2f, insetIncrement = nodeHeight * 2f;
+            float depthIncrement = nodeHeight * 2f, insetIncrement = nodeWidth * 2f;
 
             using (Graphics g = Graphics.FromImage(image))
             {
                 foreach (var node in tree.AllNodes)
                 {
-                    float xpos = node.Depth * depthIncrement, xCenter = xpos + nodeWidth / 2f;
-                    float ypos = node.RowPos * insetIncrement, yCenter = ypos + nodeHeight / 2f;
+                    float xpos = node.RowPos * insetIncrement, xCenter = xpos + nodeWidth / 2f;
+                    float ypos = node.Depth * depthIncrement, yCenter = ypos + nodeHeight / 2f;
 
                     foreach (var child in node.Unlocks)
                     {
                         g.DrawLine(linkPen, xCenter, yCenter,
-                            child.Depth * depthIncrement + nodeWidth / 2f,
-                            child.RowPos * insetIncrement + nodeHeight / 2f);
+                            child.RowPos * insetIncrement + nodeWidth / 2f,
+                            child.Depth * depthIncrement + nodeHeight / 2f);
                     }
 
                     g.FillEllipse(new SolidBrush(node.Color), xpos, ypos, nodeWidth, nodeHeight);
