cf8ea142d7ea572ec1e41e9242363d72011c7ef4
FTWinston <ftwinston@gmail.com>
2014-02-14 18:57:48 +0000

    Renamed TechTree project to GameLogic

--- GameLogic/Properties/AssemblyInfo.cs
+++ GameLogic/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TechTree")]
+[assembly: AssemblyTitle("TechTree.GameLogic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TechTree")]
+[assembly: AssemblyProduct("TechTree.GameLogic")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
