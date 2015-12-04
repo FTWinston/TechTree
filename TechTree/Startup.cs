071897160058e93b1486b290603b57b18ae2ecba
FTWinston <ftwinston@gmail.com>
2015-12-04 16:06:52 +0000

    Initial commit of template project

---
+++ TechTree/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechTree.Startup))]
+namespace TechTree
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
