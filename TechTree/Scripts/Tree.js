f4c117a1a5562678e2e1a9192553ee116c8a4481
FTWinston <ftwinston@gmail.com>
2015-12-11 11:58:39 +0000

    Displaying relationship links between buildings

--- TechTree/Scripts/Tree.js
+++ TechTree/Scripts/Tree.js
@@ -34,5 +34,44 @@ function alignTree() {
         row++;
     } while (num > 0);
 
-    $('techtree').css('width', maxX + 'px').css('height', maxY + 'px');
+    var tree = $('techtree').css('width', maxX + 'px').css('height', maxY + 'px');
+    var branchHeight = marginSize / 2;
+
+    $('techtree building[requires]').each(function (i, child) {
+        var parentSymbol = child.getAttribute('requires');
+        var parent = $('techtree building[symbol="' + parentSymbol + '"]')[0];
+
+        var childX = child.offsetLeft + child.offsetWidth / 2;
+        var childY = child.offsetTop;
+        var parentX = parent.offsetLeft + parent.offsetWidth / 2;
+        var parentY = parent.offsetTop + 3 * parent.offsetHeight / 4;
+
+        if (childX == parentX) {
+            tree.append(
+                $('<link />')
+                    .addClass('straight')
+                    .css('top', parentY + 'px')
+                    .css('left', parentX + 'px')
+                    .css('height', (childY - parentY + child.offsetHeight / 4) + 'px')
+            );
+        }
+        else {
+            tree.append(
+                $('<link />')
+                    .addClass('straight')
+                    .css('top', parentY + 'px')
+                    .css('left', parentX + 'px')
+                    .css('height', (childY - parentY - branchHeight) + 'px')
+            );
+
+            tree.append(
+                $('<link />')
+                    .addClass(childX < parentX ? 'left' : 'right')
+                    .css('top', (childY - branchHeight) + 'px')
+                    .css('left', Math.min(childX, parentX) + 'px')
+                    .css('width', Math.abs(childX - parentX - (childX < parentX ? 2 : 0)) + 'px')
+                    .css('height', (branchHeight + child.offsetHeight / 4) + 'px')
+            );
+        }
+    });
 }
