baa832f745e46e223a990fc404039466cf9e31f0
FTWinston <ftwinston@gmail.com>
2016-01-15 14:35:34 +0000

    Created a game controller, moved tree rendering into a partial, and added a simple view that displays a tech tree and a game canvas

---
+++ TechTree/Controllers/GameController.cs
@@ -0,0 +1,30 @@
+﻿using GameModels.Generation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechTree.Models;
+
+namespace TechTree.Controllers
+{
+    public class GameController : Controller
+    {
+        //
+        // GET: /Game/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Game/Test
+        public ActionResult Test()
+        {
+            var tree = TreeGenerator.Generate(TreeGenerator.Complexity.Normal);
+            var model = new GameModel() { Tree = tree };
+
+            return View("Play", model);
+        }
+	}
+}
---
+++ TechTree/Models/GameModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechTree.Models
+{
+    public class GameModel
+    {
+        public GameModels.TechTree Tree { get; set; }
+    }
+}
