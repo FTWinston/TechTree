9d6f2cf8d32def14a4bfb865fe98c7e613f4d1a3
FTWinston <ftwinston@gmail.com>
2020-02-17 00:03:06 +0000

    Simplified battlefield logic, removed Direction enum

--- GameModels/Battlefield.cs
+++ GameModels/Battlefield.cs
@@ -33,49 +33,6 @@ namespace GameModels
                 : Cells[index];
         }
 
-        private Cell GetNeighbour(Cell from, Direction towards)
-        {
-            var offset = Offsets[towards];
-            return GetCell(from.Col + offset.Col, from.Row + offset.Row);
-        }
-
-        public IEnumerable<Cell> GetNeighbours(Cell from)
-        {
-            foreach (var direction in CardinalDirections)
-            {
-                var neighbour = GetNeighbour(from, direction);
-                if (neighbour != null)
-                    yield return neighbour;
-            }
-        }
-
-        private enum Direction
-        {
-            East = 0,
-            NorthEast = 1,
-            NorthWest = 2,
-            West = 3,
-            SouthWest = 4,
-            SouthEast = 5,
-
-            Diagonal_NorthEast = 6,
-            Diagonal_North = 7,
-            Diagonal_NorthWest = 8,
-            Diagonal_SouthWest = 9,
-            Diagonal_South = 10,
-            Diagonal_SouthEast = 11,
-        }
-
-        private static readonly Direction[] CardinalDirections =
-        {
-            Direction.East,
-            Direction.NorthEast,
-            Direction.NorthWest,
-            Direction.West,
-            Direction.SouthWest,
-            Direction.SouthEast,
-        };
-
         private class Offset
         {
             public Offset(int col, int row)
@@ -88,22 +45,26 @@ namespace GameModels
             public int Row { get; }
         }
 
-        private static readonly Dictionary<Direction, Offset> Offsets = new Dictionary<Direction, Offset>
+        private static readonly Offset[] CardinalOffsets =
         {
-            { Direction.East, new Offset(1, 0) },
-            { Direction.NorthEast, new Offset(+1, -1) },
-            { Direction.NorthWest, new Offset(0, -1) },
-            { Direction.West, new Offset(-1, 0) },
-            { Direction.SouthWest, new Offset(-1, +1) },
-            { Direction.SouthEast, new Offset(0, +1) },
-
-            { Direction.Diagonal_NorthEast, new Offset(2, -1) },
-            { Direction.Diagonal_North, new Offset(1, -2) },
-            { Direction.Diagonal_NorthWest, new Offset(-1, -1) },
-            { Direction.Diagonal_SouthWest, new Offset(-2, 1) },
-            { Direction.Diagonal_South, new Offset(-1, 2) },
-            { Direction.Diagonal_SouthEast, new Offset(1, 1) },
+            new Offset(1, 0), // east
+            new Offset(+1, -1), // northeast
+            new Offset(0, -1), // northwest
+            new Offset(-1, 0), // west
+            new Offset(-1, +1), // southwest
+            new Offset(0, +1), // southeast
         };
+
+        public IEnumerable<Cell> GetNeighbours(Cell from)
+        {
+            foreach (var offset in CardinalOffsets)
+            {
+                var neighbour = GetCell(from.Col + offset.Col, from.Row + offset.Row);
+
+                if (neighbour != null)
+                    yield return neighbour;
+            }
+        }
 
         public int GetDistance(Cell from, Cell to)
         {
