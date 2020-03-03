cf96dbe2f133be78cdabd37c02e7d047b854c633
FTWinston <ftwinston@gmail.com>
2020-03-03 16:25:09 +0000

    Doing string comparisons in UTF8

--- GameModels/Serialization/GameDefinitionConverter.cs
+++ GameModels/Serialization/GameDefinitionConverter.cs
@@ -8,6 +8,13 @@ namespace ObjectiveStrategy.GameModels.Serialization
 {
     public class GameDefinitionConverter : JsonConverter<GameDefinition>
     {
+        private static readonly JsonEncodedText Seed = JsonEncodedText.Encode(nameof(GameDefinition.Seed));
+        private static readonly JsonEncodedText Complexity = JsonEncodedText.Encode(nameof(GameDefinition.Complexity));
+        private static readonly JsonEncodedText TurnLimit = JsonEncodedText.Encode(nameof(GameDefinition.TurnLimit));
+        private static readonly JsonEncodedText TechTree = JsonEncodedText.Encode(nameof(GameDefinition.TechTree));
+        private static readonly JsonEncodedText Battlefield = JsonEncodedText.Encode(nameof(GameDefinition.Battlefield));
+        private static readonly JsonEncodedText Objectives = JsonEncodedText.Encode(nameof(GameDefinition.Objectives));
+
         public override GameDefinition Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             if (reader.TokenType != JsonTokenType.StartObject)
@@ -24,49 +31,55 @@ namespace ObjectiveStrategy.GameModels.Serialization
             if (reader.TokenType != JsonTokenType.PropertyName)
                 throw new JsonException();
 
-            string propertyName = reader.GetString();
-            reader.Read();
-
-            switch (propertyName)
+            if (reader.ValueTextEquals(Seed.EncodedUtf8Bytes))
             {
-                case nameof(GameDefinition.Seed):
-                    if (reader.TokenType != JsonTokenType.Number)
-                        throw new JsonException();
-                    seed = reader.GetInt32();
-                    break;
-                case nameof(GameDefinition.Complexity):
-                    if (reader.TokenType != JsonTokenType.Number)
-                        throw new JsonException();
-                    complexity = reader.GetInt32();
-                    break;
-                case nameof(GameDefinition.TurnLimit):
-                    if (reader.TokenType != JsonTokenType.Number)
-                        throw new JsonException();
-                    turnLimit = reader.GetInt32();
-                    break;
-                case nameof(GameDefinition.Battlefield):
-                    if (reader.TokenType != JsonTokenType.StartObject)
-                        throw new JsonException();
-                    battlefield = JsonSerializer.Deserialize<Battlefield>(ref reader);
-                    break;
-                case nameof(GameDefinition.TechTree):
-                    if (reader.TokenType != JsonTokenType.StartObject)
-                        throw new JsonException();
-                    techTree = JsonSerializer.Deserialize<TechTree>(ref reader);
-                    break;
-                case nameof(GameDefinition.Objectives):
-                    if (reader.TokenType != JsonTokenType.StartArray)
-                        throw new JsonException();
-
-                    reader.Read();
-
-                    while (reader.TokenType != JsonTokenType.EndArray)
-                        objectives.Add(JsonSerializer.Deserialize<Objective>(ref reader));
-
-                    break;
-                default:
+                reader.Read();
+                if (reader.TokenType != JsonTokenType.Number)
                     throw new JsonException();
+                seed = reader.GetInt32();
             }
+            else if (reader.ValueTextEquals(Complexity.EncodedUtf8Bytes))
+            {
+                reader.Read();
+                if (reader.TokenType != JsonTokenType.Number)
+                    throw new JsonException();
+                complexity = reader.GetInt32();
+            }
+            else if (reader.ValueTextEquals(TurnLimit.EncodedUtf8Bytes))
+            {
+                reader.Read();
+                if (reader.TokenType != JsonTokenType.Number)
+                    throw new JsonException();
+                turnLimit = reader.GetInt32();
+            }
+            else if (reader.ValueTextEquals(Battlefield.EncodedUtf8Bytes))
+            {
+                reader.Read();
+                if (reader.TokenType != JsonTokenType.StartObject)
+                    throw new JsonException();
+                battlefield = JsonSerializer.Deserialize<Battlefield>(ref reader);
+            }
+            else if (reader.ValueTextEquals(TechTree.EncodedUtf8Bytes))
+            {
+                reader.Read();
+                if (reader.TokenType != JsonTokenType.StartObject)
+                    throw new JsonException();
+                techTree = JsonSerializer.Deserialize<TechTree>(ref reader);
+            }
+            else if (reader.ValueTextEquals(Objectives.EncodedUtf8Bytes))
+            {
+                reader.Read();
+
+                if (reader.TokenType != JsonTokenType.StartArray)
+                    throw new JsonException();
+
+                reader.Read();
+
+                while (reader.TokenType != JsonTokenType.EndArray)
+                    objectives.Add(JsonSerializer.Deserialize<Objective>(ref reader));
+            }
+            else
+                throw new JsonException();
 
             reader.Read(); // end the current object
 
@@ -80,7 +93,7 @@ namespace ObjectiveStrategy.GameModels.Serialization
         {
             writer.WriteStartObject();
 
-            writer.WriteNumber(nameof(GameDefinition.Seed), value.Seed);
+            writer.WriteNumber(Seed, value.Seed);
 
             writer.WriteNumber(nameof(GameDefinition.Complexity), value.Complexity);
 
